33a62d7b7ecededd9f6f132f2defc8b1d0ce7461
mrcocoball <gunmaru93@gmail.com>
2023-01-23 17:34:59 +0900

    #1

---
+++ querydsl-inflearn/src/main/java/com/querydslinflearn/QuerydslInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.querydslinflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslInflearnApplication.class, args);
+    }
+
+}
---
+++ querydsl-inflearn/src/test/java/com/querydslinflearn/QuerydslInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.querydslinflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
