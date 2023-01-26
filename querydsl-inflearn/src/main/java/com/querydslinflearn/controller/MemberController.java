ce2d565dee8d5e81425c77ee143037050902d977
mrcocoball <gunmaru93@gmail.com>
2023-01-27 00:30:24 +0900

    #3

---
+++ querydsl-inflearn/src/main/java/com/querydslinflearn/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.querydslinflearn.controller;
+
+import com.querydslinflearn.dto.MemberSearchCondition;
+import com.querydslinflearn.dto.MemberTeamDto;
+import com.querydslinflearn.repository.MemberJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.search(condition);
+    }
+
+}
